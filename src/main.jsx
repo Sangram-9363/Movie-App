f48c707ce40ea65f7df0ff55f91d6ca97e621d51
Sangram <sangramnikam5566@gmail.com>
2025-04-20 19:42:34 +0530

    Fix: Corrected store path for Vercel

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,6 @@ import App from "./App.jsx";
 import router from "./routes/index.jsx";
 import { Provider } from "react-redux";
 import Store from "./store/Store.jsx";
-
 createRoot(document.getElementById("root")).render(
   <StrictMode>
     <Provider store={Store}>
